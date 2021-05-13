a36cbdd6510917ac1d02747041b6b62e65f6909f
bhanu468 <bhanuprakashtugu2324.gmail.com>
2021-05-13 23:39:57 +0530

    First Commit

---
+++ invertedhalfpyramid.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+    int rows;
+    cin>>rows;
+    //int columns;
+    //cin>>columns;
+
+
+    for(int i=1;i<=rows;i++){
+        for(int j=1;j<=rows;j++){
+            if(i<=j){
+                cout<<"*";
+            }
+        }
+        cout<<"\n";
+    }
+
+    return 0;
+}
